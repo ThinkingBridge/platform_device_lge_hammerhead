73f092f1afdc67599ef1cc6ab508be2cad76a4ee
htchoi1995 <htchoi1995@gmail.com>
2014-02-13 01:11:09 +0900

    hammerhead: fix recovery..my fault

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,20 +115,10 @@ TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
 
 TARGET_TOUCHBOOST_FREQUENCY:= 1200
 
-#TWRP
-BOARD_HAS_NO_REAL_SDCARD := true
-RECOVERY_SDCARD_ON_DATA := true
-TW_NO_USB_STORAGE := false
-TW_INCLUDE_JB_CRYPTO := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-# The real path for this is /sys/devices/mdp.0/qcom,cmdss_fb_primary.160/leds/lcd-backlight/brightness but the comma doesn't compile correctly
-TW_BRIGHTNESS_PATH := "/sys/devices/mdp.0/qcom\x2cmdss_fb_primary.160/leds/lcd-backlight/brightness"
-TW_MAX_BRIGHTNESS := 255
-TW_NO_SCREEN_TIMEOUT := true
+# Recovery
+RECOVERY_FSTAB_VERSION := 2
+BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
-
-# Enable workaround for slow rom flash
-BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_RECOVERY_SWIPE := true
 
 -include vendor/lge/hammerhead/BoardConfigVendor.mk
